e6bd6ccef0846b436f579439fb3066203a44b2cf
tom--bo <otsuka.t.2013@gmail.com>
2016-09-21 13:49:54 +0900

    fix

--- keyboards/ergodox/keymaps/ab/keymap.c
+++ keyboards/ergodox/keymaps/ab/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  Dn  |           | Left |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   '    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  |CpsCtl| LGui | LGui | LAlt |                                       | RGui |  [   |   ]  | RAlt |   '  |
+ *   | Esc  |CpsCtl| LGui | LAlt | LGui |                                       | RGui |  [   |   ]  | RAlt |   '  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | Esc  |      |       | RESET |      |
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_UP,
 	KC_LCTL,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
 	KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_DOWN,
-	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LGUI,  KC_LALT,
+	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LALT,  KC_LGUI,
 	KC_ESC,   KC_NO,
 	KC_NO,
 	KC_SPC,   KC_DEL,  TG(FN1),
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_UP,
 	KC_LCTL,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
 	KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_DOWN,
-	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LGUI,  KC_LALT,
+	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LGUI,  KC_RALT,
 	KC_ESC,   KC_NO,
 	KC_NO,
 	KC_SPC,   KC_DEL,  TG(FN1),
