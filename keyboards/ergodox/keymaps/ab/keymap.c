e16d46c657d04f93e6c71986b67aa2a3bf2d845a
tom--bo <otsuka.t.2013@gmail.com>
2018-05-02 23:52:20 +0900

    update vol and backspace with thumb

--- keyboards/ergodox/keymaps/ab/keymap.c
+++ keyboards/ergodox/keymaps/ab/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | Esc  |      |       | RESET |      |
  *                                 ,------|------+------|       |-------+------+------.
  *                                 |      |      |      |       |       |      |      |
- *                                 |Space | Del  |------|       |-------|BSpace|Space |
+ *                                 |Space |BSpace|------|       |-------|BSpace|Space |
  *                                 |      |      | FN1  |       |  THK  |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LALT,  KC_LGUI,
 	KC_ESC,   KC_NO,
 	KC_NO,
-	KC_SPC,   KC_DEL,  TG(FN1),
+	KC_SPC,   KC_BSPC,  TG(FN1),
 	// Right hand
 	KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
 	KC_LEFT, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSLS,
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | Esc  |      |       | RESET |      |
  *                                 ,------|------+------|       |-------+------+------.
  *                                 |      |      |      |       |       |      |      |
- *                                 |Space | Del  |------|       |-------|BSpace|Space |
+ *                                 |Space |BSpace|------|       |-------|BSpace|Space |
  *                                 |      |      | FN1  |       |  THK  |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC,   CAPS_CTL,KC_LGUI, KC_LGUI,  KC_RALT,
 	KC_ESC,   KC_NO,
 	KC_NO,
-	KC_SPC,   KC_DEL,  TG(FN1),
+	KC_SPC,   KC_BSPC,  TG(FN1),
 	// Right hand
 	KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
 	KC_LEFT, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSLS,
@@ -112,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | BSpace |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|VolUp |           |      |------+------+------+------+------+--------|
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|  <-  |  ^   |  v   |  ->  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      | LClk | MClk | RClk |      |      |           |      |      |      |      |      |      |        |
+ * |        |      | LClk | MClk | RClk |      |VolDn |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Teensy|      |      |      |      |                                       | Undo |VolDn |VolUp | Mute |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -131,9 +131,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FN1] = KEYMAP(
 	// Left hand
 	KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_VOLU,
 	KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_VOLD,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS,
 	KC_TRNS,
